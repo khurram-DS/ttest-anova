e8588147a86daa9a9b8c7d058cbb39eeeb398f05
khurram shakoor <73300634+khurram-DS@users.noreply.github.com>
2022-07-25 03:50:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -360,7 +360,7 @@ The t-test is a method that determines whether two populations are statistically
                         layout = go.Layout(barmode='overlay', title="Histogram of {} for all the {}".format(selected_columns2,selected_columns1))
                         fig2 = go.Figure(data=data, layout=layout)
                         st.write(fig2)
-                        st.write("Bar plot of {} for all the {}".format(selected_columns2,selected_columns1))
+                        st.write("**Means of {} by {} with 95.00% CI Error Bars**".format(selected_columns2,selected_columns1))
                         import plotly.express as px
                         stat=df.groupby(selected_columns1)[selected_columns2].describe()
                 
